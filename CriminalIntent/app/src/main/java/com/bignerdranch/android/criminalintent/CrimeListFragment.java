531e9103cded1d04db6ef302d69bf58d38634f91
Anastasija Nikolovska <nikolovska.anastasija@gmail.com>
2017-04-18 13:16:12 +0200

    Criminal intent ch.10

---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.app.Fragment;
+import android.view.View;
+
+/**
+ * Created by Anastasija on 18-Apr-17.
+ */
+
+public class CrimeListFragment extends Fragment {
+
+   
+}
